4ec7e83795f65d6e81491d5747c1a7336033a6a7
gulperii <34350876+gulperii@users.noreply.github.com>
2020-05-31 23:37:28 +0300

    Add files via upload

--- DBtify/dbtify.py
+++ DBtify/dbtify.py
@@ -14,7 +14,8 @@ app.config['MYSQL_DB'] = 'dbtify'
 mysql = MySQL(app)
 app.secret_key = "super secret key"
 
-@app.route('/dbtify/login', methods =['GET','POST'])
+
+@app.route('/dbtify/login', methods=['GET', 'POST'])
 def login():
     msg = 'Please select your role'
     if request.method == 'POST' and 'submit' in request.form:
@@ -28,7 +29,8 @@ def login():
 
     return render_template('welcome_page.html', msg=msg)
 
-@app.route('/dbtify/loginlistener', methods=['GET','POST'])
+
+@app.route('/dbtify/loginlistener', methods=['GET', 'POST'])
 def login_listener():
     # Output message if something goes wrong...
     msg = 'Welcome to DBtify'
@@ -47,14 +49,15 @@ def login_listener():
             session['username'] = account['username']
             session['permission'] = 0
             # Redirect to home page
-            #return 'Logged in successfully!'
-            return redirect(url_for('home'))
+            # return 'Logged in successfully!'
+            return redirect(url_for('home_listener'))
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect credentails!'
     return render_template('index_listener.html', msg=msg)
 
-@app.route('/dbtify/loginartist', methods=['GET','POST'])
+
+@app.route('/dbtify/loginartist', methods=['GET', 'POST'])
 def login_artist():
     # Output message if something goes wrong...
     msg = 'Welcome to DBtify'
@@ -73,8 +76,9 @@ def login_artist():
             session['username'] = account['name']
             session['permission'] = 1
             # Redirect to home page
-            #return 'Logged in successfully!'
-            return redirect(url_for('home'))
+            # return 'Logged in successfully!'
+            cursor.close()
+            return redirect(url_for('home_artist'))
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect credentials!'
@@ -90,22 +94,130 @@ def logout():
     # Redirect to login page
     return redirect(url_for('login'))
 
-@app.route('/home')
-def home():
+
+@app.route('/homelistener')
+def home_listener():
     # Check if user is loggedin
     if 'loggedin' in session:
         # User is loggedin show them the home page
-        #return render_template('home.html', username=session['username'])
-        return render_template('home.html',username = session['username'])
+        return render_template('home_listener.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    print("i am not logged in")
+    return redirect(url_for('login'))
+
+
+@app.route('/homeartist')
+def home_artist():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        # return render_template('home.html', username=session['username'])
+        return render_template('home_artist.html', username=session['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+@app.route('/show_all_songs')
+def show_all_songs():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT title, album_id FROM songs")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('all_songs.html', data=data)
+
+
+@app.route('/show_all_artist')
+def show_all_artists():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT name, surname FROM artists")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('all_artists.html', data=data)
+
+
+@app.route('/show_all_albums')
+def show_all_albums():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT title, artist_id FROM albums")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('all_albums.html', data=data)
+
+'''
+@app.route('/show_all_songs_in_album')
+def show_all_songs_in_album(album_id):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT songs.title, songs.no_of_likes FROM songs INNER JOIN albums ON songs.album_id = albums.id;")
+
+    data = cursor.fetchall()
+    cursor.close()
+    print(data)
+    return render_template('listener_album_page.html', data=data, album=album_id)
+   
+
+
+def show_liked_songs(listener_id)
+def show_by_genre(genre)
+def show_by_keyword()
+def show_popular_artists()
+
+def show_artist_songs()
+def show_artist_albums()
+def show_artist_tops()
+def show
+def create_album()
+'''
+
+
+@app.route('/dbtify/search_album', methods=['GET', 'POST'])
+def search_album():
+    msg = "Please enter msg id"
+    if request.method == 'POST' and 'album_id' in request.form:
+        album_id = request.form['album_id']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM albums WHERE id = %s', (album_id,))
+        # Fetch one record and return result
+        album = cursor.fetchone()
+        cursor.close()
+        if album:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT songs.title, songs.no_of_likes FROM songs INNER JOIN albums ON songs.album_id = albums.id WHERE songs.album_id = %s;",(album_id,))
+            data = cursor.fetchall()
+            cursor.close()
+            return render_template('listener_album_page.html', data=data, album=album)
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect album id!'
+    return render_template('listener_album_search.html', msg=msg)
+
+@app.route('/dbtify/search_by_genre', methods=['GET', 'POST'])
+def search_by_genre():
+    msg = "Please enter msg id"
+    if request.method == 'POST' and 'genre' in request.form:
+        genre_to_search = request.form['genre']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM genres WHERE name = %s', (genre_to_search,))
+        # Fetch one record and return result
+        genre = cursor.fetchone()
+        cursor.close()
+        if genre:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT songs.title, songs.no_of_likes FROM songs INNER JOIN albums ON albums.id = songs.album_id WHERE albums.genre = %s;",(genre_to_search,))
+            data = cursor.fetchall()
+            print(data)
+            cursor.close()
+            return render_template('listener_genre_page.html', data=data, genre=genre_to_search)
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect album genre!'
+    return render_template('listener_genre_search.html', msg=msg)
 
 @app.route('/dbtify/profile')
 def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
         if session['permission'] == 0:
-        # We need all the account info for the user so we can display it on the profile page
+            # We need all the account info for the user so we can display it on the profile page
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute('SELECT * FROM listener WHERE username = %s', (session['username'],))
             account = cursor.fetchone()
@@ -121,7 +233,6 @@ def profile():
 
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
-
 
 
 '''
